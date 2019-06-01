092d0518dab446e0f4c6ab41739985b4958558da
shenliangshan <shenliangshan_hust@163.com>
2019-06-01 13:40:06 +0800

    pass test 3 and 4

--- main/main.js
+++ main/main.js
@@ -6,8 +6,12 @@ module.exports = function main(inputs) {
     {
         return Math.round(6 + parkTime * 0.25);
     }
-    else if(distance < 8)
+    else if(distance <= 8)
     {
         return Math.round(6 + (distance - 2) * 0.8 + parkTime * 0.25);
+    }
+    else
+    {
+        return Math.round(6 + 6 * 0.8 + (distance - 8) * 1.2 + parkTime * 0.25);
     }
 };
