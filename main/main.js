80e12249ab8079b8a0f1be89256053f698953760
shenliangshan <shenliangshan_hust@163.com>
2019-06-01 13:41:57 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -2,16 +2,5 @@ module.exports = function main(inputs) {
     distance = inputs['distance'];
     parkTime=  inputs['parkTime'];
 
-    if(distance <= 2)
-    {
-        return Math.round(6 + parkTime * 0.25);
-    }
-    else if(distance <= 8)
-    {
-        return Math.round(6 + (distance - 2) * 0.8 + parkTime * 0.25);
-    }
-    else
-    {
-        return Math.round(6 + 6 * 0.8 + (distance - 8) * 1.2 + parkTime * 0.25);
-    }
+    return Math.round(6 + Math.min(6 * 0.8, Math.max(0, distance - 2) * 0.8) + Math.max(0, distance - 8) * 1.2 + parkTime * 0.25);
 };
